43e2229ab0a2ed6511ea4639735f9073d662eeec
MarlosGH <101493054+MarlosGH@users.noreply.github.com>
2023-12-04 23:11:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector(".button").addEventListener("click", (e) =>{
+        window.location.href = "views/confesarme.html"
+})
+document.querySelector(".btn-inicio").addEventListener("click", (e) =>{
+        window.location.href = "../index.html";
+    })
